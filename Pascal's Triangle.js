7ccaf497ad14b9a8099ec15af920ac9acec0d404
ddeongkyu <tjdrb4230@naver.com>
2022-09-12 18:20:34 +0900

    pascals-triangle

---
+++ Pascal's Triangle.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+  let result = [];
+  if (numRows === 1) {
+    result = [[1]];
+  }
+  if (numRows === 2) {
+    result = [[1], [1, 1]];
+  }
+  if (numRows > 2) {
+    result = [[1], [1, 1]];
+    for (let i = 3; i < numRows + 1; i++) {
+      console.log("i : ", i);
+      let middle = [];
+      for (let j = 0; j <= i - 3; j++) {
+        console.log("j : ", j);
+        //  middle.push(result[i - 2][i - 3] + result[i - 2][i - 2]);
+        middle.push(result[i - 2][j] + result[i - 2][j + 1]);
+        // console.log("... : ", result[i - 2][i - 3] + result[i - 2][i - 2]);
+        // console.log("result[i - 2][i - 3] : ", result[i - 2][i - 3]);
+        // console.log("result[i - 2][i - 2] : ", result[i - 2][i - 2]);
+        console.log("middle : ", middle);
+      }
+      middle.unshift(1);
+      middle.push(1);
+      result.push(middle);
+      console.log("result : ", result);
+      //   console.log("result : ", result);
+    }
+  }
+  //  return console.log(result);
+  return result;
+};
+
+generate(6);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,1 @@
-      //   console.log(prices[i]);
+3
