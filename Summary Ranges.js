c11f08ae08ad32de7bb610f6613ad14d6a88210a
ddeongkyu <tjdrb4230@naver.com>
2022-09-15 22:26:19 +0900

    Summary_Ranges

--- Summary Ranges.js
+++ Summary Ranges.js
@@ -3,9 +3,23 @@
  * @return {string[]}
  */
 var summaryRanges = function (nums) {
-  //   for (let i = 0; i < nums.length; i++) {
-  //     console.log(Math.abs(nums[i] - nums[i + 1]));
-  //   }
+  let arr = [];
+  let start = 0;
+  let idx = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i + 1] === nums[i] + 1) {
+      idx = nums[i + 1];
+    } else if (idx > start) {
+      arr.push(`${start}->${idx}`);
+      start = nums[i + 1];
+    } else {
+      arr.push(`${start}`);
+      start = nums[i + 1];
+    }
+  }
+  console.log(idx);
+  console.log(`start : ${start}`);
+  console.log(arr);
 };
 
 console.log(summaryRanges([0, 2, 3, 4, 6, 8, 9]));
