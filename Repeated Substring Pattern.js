fb0861cafbdd562b8f9744f9bcf231395e9ba7f7
ddeongkyu <tjdrb4230@naver.com>
2022-09-21 22:09:30 +0900

    Repeated_Substring_Pattern

--- Repeated Substring Pattern.js
+++ Repeated Substring Pattern.js
@@ -3,28 +3,42 @@
  * @return {boolean}
  */
 var repeatedSubstringPattern = function (s) {
-  let idx = 0;
-  let res = false;
-  let i = 2;
+  //   let idx = 0;
+  //   let res = false;
+  //   let i = 2;
 
-  if (s.length === 3) {
-    s[0] === s[1] && s[1] === s[2] ? (res = true) : (res = false);
-  }
-  while (i < s.length) {
-    if (s[idx] === s[i]) {
-      //   console.log(s[idx]);
-      if (s[idx + 1] === s[i + 1]) {
-        return (res = true);
-      }
-      i++;
-      idx++;
-    } else if (s[idx] !== s[i]) {
-      i++;
+  //   if (s.length === 3) {
+  //     s[0] === s[1] && s[1] === s[2] ? (res = true) : (res = false);
+  //   }
+  //   while (i < s.length) {
+  //     if (s[idx] === s[i]) {
+  //       //   console.log(s[idx]);
+  //       if (s[idx + 1] === s[i + 1]) {
+  //         return (res = true);
+  //       }
+  //       i++;
+  //       idx++;
+  //     } else if (s[idx] !== s[i]) {
+  //       i++;
+  //     }
+  //   }
+  //   return res;
+  let len = s.length;
+  let currStr;
+
+  for (let i = 1; i <= len / 2; i++) {
+    currStr = s.substring(0, i);
+    console.log("first curr : ", currStr);
+    if (len % i === 0) {
+      currStr = currStr.repeat(len / i);
+      console.log("second curr : ", currStr);
+
+      if (currStr === s) return true;
     }
   }
-  return res;
+  return false;
 };
 
-console.log(repeatedSubstringPattern("ababba"));
+console.log(repeatedSubstringPattern("aaaaa"));
 
 // ababba 가 false 라는데? ab 두번 반복되잖아
