28c1d7eb542a0c4ccf0069a73937a4363e17e19c
ddeongkyu <tjdrb4230@naver.com>
2022-09-22 12:01:36 +0900

    Max_Consecutive

---
+++ Max Consecutive Ones.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function (nums) {
+  let res = 0;
+  let count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      res++;
+    } else {
+      count = Math.max(count, res);
+      res = 0;
+    }
+  }
+  res = Math.max(res, count);
+  return res;
+  console.log(Math.max(res, count));
+  console.log(`res : ${res}`);
+  console.log(`count : ${count}`);
+};
+
+console.log(findMaxConsecutiveOnes([1, 0, 1, 1, 0, 0, 1]));
