eb94d6f7dc1a88e6f5ef5e395071301a7601a84d
ddeongkyu <tjdrb4230@naver.com>
2022-09-24 20:55:29 +0900

    Distribute_Candies

---
+++ Distribute Candies.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} candyType
+ * @return {number}
+ */
+var distributeCandies = function (candyType) {
+  const maxCandy = candyType.length / 2;
+  const a = new Set([...candyType]);
+  console.log(`max candy : ${maxCandy}`);
+  console.log(candyType);
+  console.log(a);
+  return Math.min(maxCandy, a.size);
+};
+
+console.log(distributeCandies([6, 6, 6, 6]));
+console.log(distributeCandies([1, 1, 2, 2, 3, 3]));
+console.log(distributeCandies([1, 1, 2, 3]));
