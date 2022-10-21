ecdf4f2764ce7613fcc776b5ae032e4a21ea9d3d
ddeongkyu <tjdrb4230@naver.com>
2022-10-21 16:09:07 +0900

    Count_Odd_Numbers_in_an_Interval_Range

---
+++ Count Odd Numbers in an Interval Range.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function (low, high) {
+  let count = 0;
+  for (let i = low; i <= high; i++) {
+    if (i % 2 !== 0) {
+      count++;
+    }
+  }
+  return count;
+};
+
+console.log(countOdds(3, 7));
+console.log(countOdds(8, 10));
