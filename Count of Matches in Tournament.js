8c80d61556982140ac1544fcdf359f3251154d97
ddeongkyu <tjdrb4230@naver.com>
2022-10-25 18:08:58 +0900

    Count_of_Matches_in_Tournamanet

---
+++ Count of Matches in Tournament.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numberOfMatches = function (n) {
+  let ans = 0;
+  while (n > 1) {
+    if (n % 2 === 0) {
+      ans += n / 2;
+      n /= 2;
+    } else {
+      ans += (n - 1) / 2;
+      n = (n - 1) / 2 + 1;
+    }
+  }
+  console.log("ans : ", ans);
+};
+
+console.log(numberOfMatches(12));
